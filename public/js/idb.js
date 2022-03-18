3b3ed411b756e3f384c3285744f40e75a61a4768
Sodaman <mattrick2214@gmail.com>
2022-03-18 03:40:22 -0500

    idb.js is done but not working, looking at it

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,7 +8,7 @@ request.onupgradeneeded = function(event) {
 
 request.onsuccess = function(event) {
     db = event.target.result;
-    if (navigator.online) {
+    if (navigator.onLine) {
         uploadBudget();
     }
 }
@@ -18,10 +18,47 @@ request.onerror = function(event) {
 }
 
 function saveBudget(budget) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
 
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    budgetObjectStore.add(budget);
 }
 
 function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: 'application/json, text/plain, */*',
+                  'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_budget'], 'readwrite');
+                const budgetObjectStore = transaction.objectStore('new_budget');
+                // clear all items in your store
+                budgetObjectStore.clear();
+              })
+              .catch(err => {
+                // set reference to redirect back here
+                console.log(err);
+
+            })
+        }
+    }
 
 }
 
